365d8fa39aec42a4d8ff3bc0f98a352152b2b11f
Abhishek Raj Ghimire <ghimireavishek634@yahoo.com>
2026-06-22 02:06:25 +1000

    Update version tag to v4.0

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from PyQt6.QtWidgets import QApplication
 from pet_window import DesktopPet
 from updater import start_updater
 
-APP_VERSION = "v1.1.0"
+APP_VERSION = "v4.0"
 
 def cleanup_old_updates():
     if getattr(sys, 'frozen', False):
